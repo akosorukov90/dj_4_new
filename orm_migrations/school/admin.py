449e428809f069964856af08da2732248c1c76e8
aleksandr <akosorukov90@gmail.com>
2021-10-26 14:00:06 +0300

    error. Teacher None

---
+++ orm_migrations/school/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from .models import Student, Teacher
+
+
+class StudentTeacherInline(admin.TabularInline):
+    model = Student.teacher.through
+    extra = 2
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    inlines = [
+        StudentTeacherInline,
+    ]
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    inlines = [
+        StudentTeacherInline,
+    ]
+
