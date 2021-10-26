8b26923b108b19143a7574121cdb1a8f19811971
aleksandr <akosorukov90@gmail.com>
2021-10-26 15:44:52 +0300

    complete

--- orm_migrations/school/views.py
+++ orm_migrations/school/views.py
@@ -7,7 +7,7 @@ from .models import Student
 def students_list(request):
     template = 'school/students_list.html'
     ordering = 'group'
-    students = Student.objects.all().order_by(ordering)
+    students = Student.objects.all().prefetch_related('teacher').order_by(ordering)
     context = {'object_list': students}
 
     # используйте этот параметр для упорядочивания результатов
